c9174b5f063614fd11610496ff890a5898a899bb
progserega <progserega@gmail.com>
2013-10-20 19:32:10 +1100

    убрал лишний код. Заработал поиск тегов.

--- osmpatch.c
+++ osmpatch.c
@@ -73,8 +73,8 @@ int str_cmp_ext(char *where_search, char *what_search, bool no_case_sensitive, b
 	what_search_wchar=(wchar_t*)malloc( (strlen(what_search)+1) * sizeof(wchar_t));
 	if(where_search_wchar!=NULL && what_search_wchar!=NULL)
 	{
-		if(mbstowcs(where_search_wchar,where_search,strlen(where_search))!=-1 &&
-		   mbstowcs(what_search_wchar,what_search,strlen(what_search))!=-1)
+		if(mbstowcs(where_search_wchar,where_search,strlen(where_search)+1)!=-1 &&
+		   mbstowcs(what_search_wchar,what_search,strlen(what_search)+1)!=-1)
 		   {
 				return_value=wc_cmp_ext(where_search_wchar,what_search_wchar,no_case_sensitive,full_match);
 		   }
@@ -275,11 +275,13 @@ int patch_by_rules(xmlNode * osm, xmlNode *rules)
 		xmlNode * cur_osm_tag = NULL;
 		xmlNode * cur_node = NULL;
 		xmlAttr * cur_prop = NULL;
+		xmlChar * rules_str = NULL;
+		xmlChar * osm_str = NULL;
 		int types_to_find=TYPE_NODE|TYPE_WAYS;
 		bool find_osm_element_to_process=FALSE;
 		int tags_rules_equal=0;
 		/// default search options for keys and values:
-		bool case_sensitive=SEARCH_OPTIONS_CASE_SENSITIVE_DEFAULT_VALUE;
+		bool no_case_sensitive=SEARCH_OPTIONS_CASE_SENSITIVE_DEFAULT_VALUE;
 		bool full_match=SEARCH_OPTIONS_FULL_MATCH_DEFAULT_VALUE;
 
 
@@ -380,18 +382,20 @@ int patch_by_rules(xmlNode * osm, xmlNode *rules)
 						
 						// process current rules tag:
 						cur_node=find_tag(cur_rules_tag->children,"key");
+
+						// process key:
 						//get search options for this key:
-						case_sensitive=SEARCH_OPTIONS_CASE_SENSITIVE_DEFAULT_VALUE;
+						no_case_sensitive=SEARCH_OPTIONS_CASE_SENSITIVE_DEFAULT_VALUE;
 						full_match=SEARCH_OPTIONS_FULL_MATCH_DEFAULT_VALUE;
 						cur_prop=cur_node->properties;
 						while(cur_prop)
 						{
-							if(strcmp(cur_prop->name,"case_sensitive")==0)
+							if(strcmp(cur_prop->name,"no_case_sensitive")==0)
 							{
 								if(strcmp(cur_prop->children->content,"yes")==0)
-									case_sensitive=TRUE;
+									no_case_sensitive=TRUE;
 								else
-									case_sensitive=FALSE;
+									no_case_sensitive=FALSE;
 							}
 							if(strcmp(cur_prop->name,"full_match")==0)
 							{
@@ -402,12 +406,59 @@ int patch_by_rules(xmlNode * osm, xmlNode *rules)
 							}
 							cur_prop=cur_prop->next;
 						}
-						// cmp current osm tag key with current rules key:
-						//if(strcmp())
+						
+						cur_prop=properties;
+						while(cur_prop)
+						{
+							if(!strcmp(cur_prop->name,"k"))
+							{
+								// cmp current osm tag key with current rules key:
+								if(cur_prop->children)
+								{
+									osm_str=cur_prop->children->content;
+									break;
+								}
+								else
+								{
+									fprintf(stderr,"%s:%i: error processing XML! 'k' attr have no content!",__FILE__,__LINE__);
+									return -1;
+								}
+							}
+							cur_prop=cur_prop->next;
+						}
+
+						if(cur_node->children)
+						{
+							rules_str=cur_node->children->content;
+						}
+						else
+						{
+							fprintf(stderr,"%s:%i: error processing XML! 'key' tag have no content!",__FILE__,__LINE__);
+							return -1;
+						}
+						if(rules_str!=NULL && osm_str!=NULL)
+						{
+							if(str_cmp_ext(osm_str,rules_str,no_case_sensitive,full_match)==0)
+							{
+								// find tag!
+#ifdef DEBUG
+								fprintf(stderr,"find tag!: Find '%s' in '%s' - now will be test value of tag...\n", rules_str, osm_str);  
+#endif
+							}
+							else
+							{
+#ifdef DEBUG
+								fprintf(stderr,"Not find tag!: Not find '%s' in '%s' - skip this tag\n", rules_str, osm_str);  
+#endif
+								cur_rules_tag=cur_rules_tag->next;
+								continue;
+							}
+						}
 
 
 
-						cur_node=find_tag(cur_rules_tag->children,"value");
+
+						// process value
 	
 
 
@@ -418,281 +469,7 @@ int patch_by_rules(xmlNode * osm, xmlNode *rules)
 				}
 				cur_osm_tag=cur_osm_tag->next;
 			}
+			cur_osm_element=cur_osm_element->next;
 		}
-
-/*
-
-					name=xmlNodeGetContent(cur_node);  
-				}
-				else if (strcmp(cur_node->name,"description")==0)
-				{
-#ifdef DEBUG
-					fprintf(stderr,"description: %s\n", xmlNodeGetContent(cur_node));  
-#endif
-					description=xmlNodeGetContent(cur_node);  
-				}
-				else if (strcmp(cur_node->name,"Polygon")==0)
-				{
-					GeoObject_type=GEO_POLY;
-					parsing_Polygon(cur_node->children);
-				}
-				else if (strcmp(cur_node->name,"LineString")==0)
-				{
-					GeoObject_type=GEO_LINE;
-					parsing_Polygon(cur_node->children);
-				}
-				else if (strcmp(cur_node->name,"Point")==0)
-				{
-					GeoObject_type=GEO_POINT;
-					parsing_Polygon(cur_node->children);
-				}
-			*/
-		/*
-		// show current GeoObject as OSM:
-		
-		if(GeoObject_type==GEO_POLY||GeoObject_type==GEO_LINE)
-		{
-				//1. show points 
-				if(!posList_num)
-				{
-						fprintf(stderr,"%s:%i: No coordinates for %s! Skip this object!\n",__FILE__,__LINE__,name);
-						free_posList(posList);
-						return -1;
-				}
-				posList_cur=posList;
-				while(posList_cur)
-				{
-					if(!posList_cur->link)
-					{
-							fprintf(stdout,"\
-  <node id='%i' action='modify' visible='TRUE' lat='%Lf' lon='%Lf' />\n",posList_cur->id,posList_cur->lat,posList_cur->lon);
-					}
-		#ifdef DEBUG
-					else
-						fprintf(stderr,"%s:%i: Found link!\n",__FILE__,__LINE__);
-		#endif
-					posList_cur=posList_cur->next;
-				}
-				// 2. show head way:
-				fprintf(stdout,"\
-		  <way id='%i' action='modify' visible='TRUE'>\n",cur_global_id);
-				// 3. show links to nodes in current way:
-				posList_cur=posList;
-				while(posList_cur)
-				{
-					fprintf(stdout,"<nd ref='%i' />\n",posList_cur->id);
-					posList_cur=posList_cur->next;
-				}
-				// 4. show tags:
-				// TODO: add dinamical translate tags
-				if(description)
-					fprintf(stdout,"    <tag k='description' v='%s' />\n",description);
-				if(name)
-					fprintf(stdout,"    <tag k='name' v='%s' />\n",name);
-				if(note)
-					fprintf(stdout,"    <tag k='note' v='%s' />\n",note);
-				if(operator)
-					fprintf(stdout,"    <tag k='operator' v='%s' />\n",operator);
-				// add type by name:
-				if(!strncmp("ПС",name,sizeof("ПС")-1))
-				{
-						fprintf(stdout,"    <tag k='power' v='station' />\n");
-				}
-				else if(!strncmp("КВЛ",name,sizeof("КВЛ")-1)||!strncmp("ВЛ",name,sizeof("ВЛ")-1)||!strncmp("КЛ",name,sizeof("КЛ")-1))
-				{
-						fprintf(stdout,"    <tag k='power' v='line' />\n");
-				}
-
-				//<tag k='ref' v='№45' />
-				// 5. close GeoObject object:
-				fprintf(stdout,"  </way>\n");
-				cur_global_id--;
-		}
-		else if(GeoObject_type==GEO_POINT)
-		{
-				if(posList_num<1)
-				{
-					fprintf(stderr,"%s:%i: No coordinates for POI - skip!\n",__FILE__,__LINE__);
-					return -1;
-				}
-				//1. show points 
-				posList_cur=posList;
-				fprintf(stdout,"\
-  <node id='%i' action='modify' visible='TRUE' lat='%Lf' lon='%Lf' >\n",posList_cur->id,posList_cur->lat,posList_cur->lon);
-					
-				// 2. show tags:
-				// TODO: add dinamical translate tags
-				if(description)
-					fprintf(stdout,"    <tag k='description' v='%s' />\n",description);
-				if(name)
-					fprintf(stdout,"    <tag k='name' v='%s' />\n",name);
-				if(note)
-					fprintf(stdout,"    <tag k='note' v='%s' />\n",note);
-				if(operator)
-					fprintf(stdout,"    <tag k='operator' v='%s' />\n",operator);
-				// add type by name:
-				if(!strncmp("ПС",name,sizeof("ПС")-1))
-				{
-						fprintf(stdout,"    <tag k='power' v='station' />\n");
-				}
-				else if(!strncmp("КВЛ",name,sizeof("КВЛ")-1)||!strncmp("ВЛ",name,sizeof("ВЛ")-1))
-				{
-						fprintf(stdout,"    <tag k='power' v='line' />\n");
-				}
-
-				//<tag k='ref' v='№45' />
-				// 5. close GeoObject object:
-				fprintf(stdout,"  </node>\n");
-		}
-		// 5. free posList:
-		free_posList(posList);
-		*/
 		return 0;
-}
-static void parsing_Polygon(xmlNode * a_node)
-{
-	xmlNode *cur_node = NULL;
-	xmlChar *text = NULL;
-	xmlChar *cur_text = NULL;
-	xmlChar lat[255];
-	xmlChar lon[255];
-	xmlChar* cur_coord=NULL;
-	int lat_success=FALSE;
-	int lon_success=FALSE;
-	int index=0;
-
-	for (cur_node = a_node; cur_node; cur_node = cur_node->next) 
-	{  
-		if (cur_node->type == XML_ELEMENT_NODE)
-		{
-				if (strcmp(cur_node->name,"posList")==0||strcmp(cur_node->name,"pos")==0)
-				{
-					// Берём координаты полигона:
-					text=xmlNodeGetContent(cur_node);
-					if(parsing_posList(text)==-1)
-					{
-						fprintf(stderr,"%s:%i: Error parsing_posList()!\n",__FILE__,__LINE__);
-					}
-				}
-				parsing_Polygon(cur_node->children);
-		}
-	}
-}
-
-int parsing_posList(xmlChar * text)
-{
-	xmlNode *cur_node = NULL;
-	xmlChar *cur_text = NULL;
-	xmlChar lat[255];
-	xmlChar lon[255];
-	xmlChar* cur_coord=NULL;
-	int lat_success=FALSE;
-	int lon_success=FALSE;
-	int index=0;
-	if(*posList_last)
-	{
-		fprintf(stderr,"%s:%i: Error! List not free! (posList_last!=NULL) It is memmory leak!\n",__FILE__,__LINE__);
-		return -1;
-	}
-			
-	cur_coord=lon;
-	for(cur_text=text;cur_text<=text+strlen(text);cur_text++)
-	{
-		if(*cur_text==' '||*cur_text==0)
-		{
-			// переключатель на другую координату:
-			if(!lon_success)
-			{
-				lon_success=TRUE;
-				*cur_coord=0;
-				cur_coord=lat;
-				index=0;
-				continue;
-			}
-			else
-			{
-				lat_success=TRUE;
-				*cur_coord=0;
-				index=0;
-			}
-		}
-		if(!lat_success || !lon_success)
-		{
-			*cur_coord=(*cur_text);
-			cur_coord++;
-			index++;
-			if(index>=255)
-			{
-				fprintf(stderr,"%s:%i: massiv error!\n",__FILE__,__LINE__);
-				break;
-			}
-		}
-		else
-		{
-			// устанавливаем переменные для выбора второй пары:
-			index=0;
-			cur_coord=lon;
-			lat_success=FALSE;
-			lon_success=FALSE;
-			//cur_text--;
-
-			// Формируем связный список из полученных точек:
-			// выделяем память под элемент списка:
-			*posList_last=malloc(sizeof(struct pointList));
-			if(!*posList_last)
-			{
-				fprintf(stderr,"%s:%i: Error aalocate %i bytes!\n",__FILE__,__LINE__,sizeof(struct pointList));
-				break;
-			}
-			memset(*posList_last,0,sizeof(struct pointList));
-			posList_num++;
-			// Заполняем структуру:
-			sscanf(lat,"%Lf",&((*posList_last)->lat));
-			sscanf(lon,"%Lf",&((*posList_last)->lon));
-			(*posList_last)->id=cur_global_id;
-			cur_global_id--;
-			// проверяем, были ли такие же точки в данной записи (закольцованность)
-			posList_cur=posList;
-			while(posList_cur)
-			{
-				if( (*posList_last)->lat==posList_cur->lat & (*posList_last)->lon==posList_cur->lon & *posList_last !=posList_cur  )
-				{	
-					(*posList_last)->id=posList_cur->id;
-					(*posList_last)->link=TRUE;
-					// отменяем уменьшение идентификатора на копию точки:
-					cur_global_id++;
-#ifdef DEBUG
-					fprintf(stderr,"%s:%i: Found link!\nid=%i",__FILE__,__LINE__,posList_cur->id);
-#endif
-					break;
-					
-				}
-				posList_cur=posList_cur->next;
-			}
-#ifdef DEBUG
-			fprintf(stderr,"str lat: %s\n",lat);
-			fprintf(stderr,"float lat: %Lf\n",(*posList_last)->lat);
-			fprintf(stderr,"str lon: %s\n",lon);
-			fprintf(stderr,"float lon: %Lf\n",(*posList_last)->lon);
-			fprintf(stderr,"id: %i\n",(*posList_last)->id);
-#endif
-			posList_last=&(*posList_last)->next;
-
-		}
-	}
-	return 0;
-}
-void free_posList(struct pointList *List)
-{
-	struct pointList *tmp;
-	while(List)
-	{
-		tmp=List;
-		List=List->next;
-		free(tmp);
-	}
-	posList=0;
-	posList_num=0;
-	posList_last=&posList;
-	posList_cur=0;
 }

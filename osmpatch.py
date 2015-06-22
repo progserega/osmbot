1ce57c3ad03bf1920961b51eac0a15fbef0a3f03
progserega <progserega@gmail.com>
2015-06-22 23:04:13 +1000

    часть кода

--- osmpatch.py
+++ osmpatch.py
@@ -3,45 +3,10 @@
 #
 from lxml import etree
 import sys
+import re
 
 #DEBUG=False
 DEBUG=True
-
-if len(sys.argv) < 3:
-	print("Необходимо два аргумента - входной файл и выходной файл")
-	sys.exit(1)
-
-def add_patchset(osmpatch, name, tags):
-	patchset= etree.SubElement(osmpatch, "patchset")
-
-	# Что ищем:
-	find = etree.SubElement(patchset, "find")
-	type_node=etree.Element("type", nodes="totally", ways="yes", relation="yes")
-	find.append(type_node)
-
-	# key1=value1
-	tag = etree.SubElement(find, "tag")
-	key=etree.Element("key", full_match="yes")
-	key.text="power"
-	tag.append(key)
-	value=etree.Element("value", full_match="yes", case_sensitive='yes')
-	value.text="station"
-	tag.append(value)
-
-	# key2=value2
-	tag = etree.SubElement(find, "tag")
-	key=etree.Element("key", full_match="yes")
-	key.text="name"
-	tag.append(key)
-	value=etree.Element("value", full_match="yes", case_sensitive='yes')
-	value.text=name
-	tag.append(value)
-
-	# На что меняем:
-	add = etree.SubElement(patchset, "add")
-	for k in tags:
-		key=etree.Element("tag", k=k, v=tags[k])
-		add.append(key)
 
 def patch(element, rules_root):
 	# Перебираем все patchset-ы:
@@ -54,78 +19,146 @@ def process_patchset(element, patchset):
 	# Перебираем все правила:
 	for rule in patchset:
 		if rule.tag=="find":
-			process_find(element,rule)
+			if process_find(element,rule):
+				if DEBUG:
+					print("Найден элемент")
+				# TODO добавляем обрабочик для успешно найденного элемента:
 
 def process_find(element, rule):
+	if DEBUG:
+		print("DEBUG: process_find")
 	find_ways=True
 	find_nodes=True
 	find_relations=True
 	# Ищем настройки, если есть:
 	for find_rule in rule:
-		if find_rule.taga=="type":
+		if find_rule.tag=="type":
 			# берём параметры поиска:
-			for key in find_rule.keys:
+			for key in find_rule.keys():
 				if key=="nodes":
-					if find_rule.get[key].lower()=="no":
+					if find_rule.get(key).lower()=="no":
 						find_nodes=False
 				if key=="ways":
-					if find_rule.get[key].lower()=="no":
+					if find_rule.get(key).lower()=="no":
 						find_ways=False
 				if key=="relations":
-					if find_rule.get[key].lower()=="no":
+					if find_rule.get(key).lower()=="no":
 						find_relations=False
-	if element.tag="node" and not find_nodes:
+	if element.tag=="node" and not find_nodes:
 		# Пропуск элемента:
-		return 0
-	if element.tag="way" and not find_ways:
+		return False
+	if element.tag=="way" and not find_ways:
 		# Пропуск элемента:
-		return 0
-	if element.tag="relation" and not find_relations:
+		return False
+	if element.tag=="relation" and not find_relations:
 		# Пропуск элемента:
-		return 0
+		return False
 
 	# Перебираем все соответствия:
 	tags=0
 	success=0
 	for find_rule in rule:
 		if find_rule.tag=="tag":
-			tags+=1
-			success+=process_tag(element, find_rule)
-			# FIXME
+			if not find_rule_tag_in_osm_element(element, find_rule):
+				# Данная тег-значение не найдена в элементе OSM, поэтому
+				# считаем, что данный элемент OSM не удовлетворяет данному правилу поиска:
+				return False
+	# Считаем, что данный элемент OSM соответствует всем искомым правилам:
+	return True
 
-def process_tag(element, tags):
+
+def find_rule_tag_in_osm_element(element, tags):
+	if DEBUG:
+		print("DEBUG: find_rule_tag_in_osm_element")
+	# По умолчанию считаем, что ищем только имя тега OSM, без его значения:
+	find_value=False
+	# Берём параметры поиска:
 	for tag in tags:
-		if tag.tag="key":
-			full_match=True
-			regex=False
-			case_sensitive=True
-			math="no"
+		if tag.tag=="key":
 			# Берём параметры:
-			for param_key in tag.keys:
-				if param_key=="full_match":
-					if tag.get[param_key].lower()=="no":
-						full_match=False
-				if param_key=="regex":
-					if tag.get[param_key].lower()=="yes":
-						regex=True
-				if param_key=="case_sensitive":
-					if tag.get[param_key].lower()=="no":
-						case_sensitive=False
-				if param_key=="math":
-					math=tag.get[param_key].lower()
-			# Ищем в элементе OSM тег, имя которого соответствует данному key:
-			for osm_tag in element:
-				if len(osm_tag):
-					k=osm_tag.get('k')
+			key_opt=get_rule_tag_param(tag)
+			# Берём искомое значение:
+			key_rule_text=tag.text
+		if tag.tag=="value":
+			find_value=True
+			# Берём параметры:
+			val_opt=get_rule_tag_param(tag)
+			# Берём искомое значение:
+			val_rule_text=tag.text
 
-		if tag.tag="value":
+		# Перебираем все теги в OSM-элементе:
+		for osm_tag in element:
+			if len(osm_tag):
+				# Для каждого тега:
+				if osm_tag.tag=="tag":
+					# Берём имя тега:
+					src_text=osm_tag.get('k')
+					# Проверяем имя тега OSM на соответствие искомому тегу правил:
+					if check_param_by_rule(src_text, key_rule_text, key_opt):
+						# Если тег совпал с искомым, проверяем, если нужно значние тега:
+						if find_value:
+							src_text=osm_tag.get('v')
+							# Проверяем значение тега OSM на соответствие значению искомого тега правил:
+							if check_param_by_rule(src_text, val_rule_text, val_opt):
+								# совпало, значит сообщаем об успехе:
+								return True
+						else:
+							# Т.к. значение искать не нужно, то сообщаем об успехе:
+							return True
+	# Не нашли теги в элементе:
+	return False
+						
 
+# Берём настройки поиска для тега правил:
+def get_rule_tag_param(tag):
+	opt={}
+	opt["regex"]=False
+	opt["math"]="no"
+	opt["case_sensitive"]=True
+	opt["full_match"]=True
 
-			
-											
+	# Берём параметры:
+	for param_key in tag.keys():
+		if param_key=="full_match":
+			if tag.get(param_key).lower()=="no":
+				opt["full_match"]=False
+		if param_key=="regex":
+			if tag.get(param_key).lower()=="yes":
+				opt["regex"]=True
+		if param_key=="case_sensitive":
+			if tag.get(param_key).lower()=="no":
+				opt["case_sensitive"]=False
+		if param_key=="math":
+			opt["math"]=tag.get(param_key).lower()
+	return opt
 
-
-
+# Проверяем значение на соответствие правилу поиска:
+def check_param_by_rule(src_text, rule_text, opt):
+	if DEBUG:
+		print("ищем '%s' в '%s'" % (rule_text, src_text))
+	if opt["regex"]:
+		if re.search(rule_text, src_text) is not None:
+			if DEBUG:
+				print("src_text='%s' соответствует регулярному выражению: '%s'" % (src_text,rule_text))
+			return True
+	elif opt["math"] !="no":
+		if opt["math"]=="lt":
+			if float(src_text) < float(rule_text):
+				return True
+		elif opt["math"]=="gt":
+			if float(src_text) > float(rule_text):
+				return True
+	if opt["case_sensitive"]:
+		src_text=src_text.lower() 
+		rule_text=rule_text.lower() 
+	if opt["full_match"]:
+		if src_text==rule_text:
+			return True
+	else:
+		if rule_text in src_text:
+			return True
+	# Не соответствует:
+	return False
 
 	
 
@@ -133,6 +166,13 @@ def process_tag(element, tags):
 nodes={}
 ways={}
 relations={}
+
+if len(sys.argv) < 4:
+	print("Необходимо три аргумента - входной файл OSM, файл правил и выходной файл OSM")
+	print("Например:")
+	print("	%s in.osm rules.xml out.xml" % sys.argv[0])
+	sys.exit(1)
+
 # Формируем структуру патчсета:
 osmpatch = etree.Element("osmpatch")
 
@@ -160,37 +200,13 @@ for node in osm_root:
 		print (node.keys())
 		relations[node.get("id")]=node
 
-
-		
-	if node.tag=="old":
-	
-		for record in node:
-			name=""
-			tags={}
-			for item in record:
-				if DEBUG:
-					print item.tag, item.text
-				if item.tag==u"Подстанция":
-					name=item.text
-				elif item.tag==u"ДатаСреза":
-					tags["power_usage_date"]=u"КДЗ %s" % item.text
-					tags["power_usage_comment"]=u"С учётом перспективной нагрузки на %s" % item.text
-				elif item.tag==u"РезЗамеровМВт":
-					tags["power_usage_mvt_real_kdz"]=item.text.replace(",",".")
-				elif item.tag==u"ДопустимаяНагрузкаМВт":
-					tags["power_usage_mvt_dopustima"]=item.text.replace(",",".")
-				elif item.tag==u"ЗагрузкаПСПроц":
-					tags["power_usage_percent"]=item.text.replace(",",".")
-				elif item.tag==u"НагрВсего":
-					tags["power_usage_mvt_perspectiv_all"]=item.text.replace(",",".")
-			if name=="":
-				print("error! name=''")
-				sys.exit(1)
-			add_patchset(osmpatch,name,tags)
-			
 # Проверяем последовательно все элементы:
 for node_id in nodes:
 	patch(nodes[node_id], rules_root)
+for way_id in ways:
+	patch(ways[way_id], rules_root)
+for relation_id in relations:
+	patch(relations[relation_id], rules_root)
 
 
 
@@ -204,7 +220,7 @@ if DEBUG:
 	print (etree.tostring(osmpatch,pretty_print=True, encoding='unicode'))
 
 string=etree.tostring(osmpatch, xml_declaration=True, encoding='UTF-8', pretty_print=True )
-f=open(sys.argv[2],"w+")
+f=open(sys.argv[3],"w+")
 f.write(string)
 f.close
 
